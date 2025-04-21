1cec2e93e7bffd6ea189e9b115f1066bfc883c46
alan-vinzant <avinzant1@gmail.com>
2025-04-21 09:51:37 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -80,7 +80,7 @@ const CONFIG = {
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      'https://github.com/alan-vinzant/alan-vinzant.github.io/rob/main/VinzantCV.pdf', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
     'PHP',
